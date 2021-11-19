ffff931f423547eb01d4029b7cf8a86c95cfeedb
veda sravya challa <16086434+sravya27@users.noreply.github.com>
2021-11-19 03:31:57 -0500

    Add files via upload

---
+++ Commands.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiValueDictionary
+{
+    public static class Commands
+    {
+        private static Dictionary<string, HashSet<string>> multiDictionary = new Dictionary<string, HashSet<string>>();
+        
+        public static bool Add(string key, string value)
+        {
+            if (multiDictionary.ContainsKey(key))
+            {
+                if (!multiDictionary[key].Contains(value))
+                {
+                    multiDictionary[key].Add(value);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("member already exists for key");
+                    return false;
+                }
+            }
+            else
+            {
+                multiDictionary.Add(key, new HashSet<string>(){value});
+                return true;
+            }
+        }
+
+        public static void PrintKeys()
+        {
+            var keys = multiDictionary.Keys.ToList();
+            foreach (var k in keys)
+            {
+                Console.WriteLine(k);
+            }
+            if(keys.Count==0) Console.WriteLine("No Keys to print");
+        }
+
+        public static bool MemberExists(string key, string value)
+        {
+            if (multiDictionary.ContainsKey(key))
+            {
+                return multiDictionary[key].Contains(value);
+            }
+
+            return false;
+        }
+
+        public static void PrintAllMembers()
+        {
+            foreach (var key in multiDictionary.Keys)
+            {
+                foreach (var value in multiDictionary[key])
+                {
+                    Console.WriteLine(value);
+                }
+            }
+
+            if (multiDictionary.Count==0) Console.WriteLine("No Members to print");
+        }
+
+        public static bool KeyExists(string key)
+        {
+            return multiDictionary.ContainsKey(key);
+        }
+        
+        public static void PrintMembers(string key)
+        {
+            if (multiDictionary.ContainsKey(key))
+            {
+                foreach (var value in multiDictionary[key])
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            Console.WriteLine("Key doesn't exist");
+        }
+
+        public static void Remove(string key, string value)
+        {
+            if (multiDictionary.ContainsKey(key))
+            {
+                if (multiDictionary[key].Contains(value))
+                {
+                    multiDictionary[key].Remove(value);
+                    if(multiDictionary[key].Count==0) multiDictionary.Remove(key);
+                    return;
+                }
+                Console.WriteLine("member doesn't exist");
+                return;
+            }
+
+            Console.WriteLine("key doesn't exist");
+        }
+
+        public static void RemoveAll(string key)
+        {
+            if (multiDictionary.ContainsKey(key))
+            {
+                multiDictionary[key].Clear();
+                multiDictionary.Remove(key);
+                return;
+            }
+
+            Console.WriteLine("key doesn't exist");
+        }
+
+        public static void Clear()
+        {
+            multiDictionary.Clear();
+        }
+
+        public static void Items()
+        {
+            foreach (var key in multiDictionary.Keys)
+            {
+                foreach (var value in multiDictionary[key])
+                {
+                    Console.WriteLine("{0}: {1}",key, value);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MultiValueDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command="",key = "",value = "";
+            Console.WriteLine("Enter exit to quit");
+            Console.WriteLine("Enter one of the following commands to perform the desired operation: ");
+            PrintMenu();
+            do
+            {
+                Console.WriteLine("Enter one of the menu commands to perform the desired operation: ");
+                
+                command = Console.ReadLine().ToLower();
+                switch (command)
+                {
+                    case "add":
+                        Console.WriteLine("Enter Key");
+                        key = Console.ReadLine();
+                        Console.WriteLine("Enter Value");
+                        value = Console.ReadLine();
+                        var success = Commands.Add(key, value);
+                        if(success) Console.WriteLine("Added");
+                        break;
+                    case "keys":
+                        Commands.PrintKeys();
+                        break;
+                    case "memberexists":
+                        Console.WriteLine("Enter Key");
+                        key = Console.ReadLine();
+                        Console.WriteLine("Enter Value");
+                        value = Console.ReadLine();
+                        Console.WriteLine(Commands.MemberExists(key, value));
+                        break;
+                    case "allmembers":
+                        Commands.PrintAllMembers();
+                        break;
+                    case "keyexists":
+                        Console.WriteLine("Enter Key");
+                        key = Console.ReadLine();
+                        Console.WriteLine(Commands.KeyExists(key));
+                        break;
+                    case "members":
+                        Console.WriteLine("Enter Key");
+                        key = Console.ReadLine();
+                        Commands.PrintMembers(key);
+                        break;
+                    case "remove":
+                        Console.WriteLine("Enter Key");
+                        key = Console.ReadLine();
+                        Console.WriteLine("Enter Value");
+                        value = Console.ReadLine();
+                        Commands.Remove(key, value);
+                        break;
+                    case "removeall":
+                        Console.WriteLine("Enter Key");
+                        key = Console.ReadLine();
+                        Commands.RemoveAll(key);
+                        break;
+                    case "clear":
+                        Commands.Clear();
+                        break;
+                    case "items":
+                        Commands.Items();
+                        break;
+                    case "menu":
+                        PrintMenu();
+                        break;
+                    default:
+                        break;
+                }
+            } while (command != "exit");
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Menu --> To display the menu ");
+            Console.WriteLine("Add --> To Add an element to the dictionary ");
+            Console.WriteLine("Keys --> To print all the elements in the dictionary ");
+            Console.WriteLine("MemberExists --> To Check if the member exists in the dictionary for the given key");
+            Console.WriteLine("AllMembers --> To print all the members in the dictionary ");
+            Console.WriteLine("keyexists --> To check if the key exists in the dictionary ");
+            Console.WriteLine("Members --> To print all the members in the dictionary ");
+            Console.WriteLine("Remove --> To remove a member from the dictionary ");
+            Console.WriteLine("RemoveAll --> To remove all the members for a key in the dictionary ");
+            Console.WriteLine("Clear --> To remove all the keys and members from the dictionary ");
+            Console.WriteLine("Items --> To print all the keys and the members in the dictionary ");
+        }
+    }
+}
